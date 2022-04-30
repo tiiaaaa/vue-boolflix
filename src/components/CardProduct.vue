496daa715e9d0940c539a485866bf40324ae0659
MattiaPortanti <mattia.portantipm@gmail.com>
2022-04-30 18:13:41 +0200

    istallata libreria vue countryflag e importata in main.js così da poter essere utilizzata in ogni component, aggiunta voce 'language' alle card e implementata funzione getLanguage() che restituisce le bandiere anche alle lingue che di default non le hanno

--- src/components/CardProduct.vue
+++ src/components/CardProduct.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="card col-12 col-md-6 col-lg-3 mb-5 py-3 border border-3">
-        <img :src="`https://image.tmdb.org/t/p/w342${element.poster_path}`" class="card-img-top img-fluid" alt="">
+        <img v-if="element.poster_path" :src="`https://image.tmdb.org/t/p/w342${element.poster_path}`" class="card-img-top img-fluid" :alt="getTitle()">
+        <img v-else src="https://dummyimage.com/600x400/8f878f/fff.png&text=Poster+not+avalaible" class="card-img-top img-fluid" :alt="getTitle()">
         <div class="card-body">
             <h2 class="card-title">{{getTitle()}}</h2>
             <p>{{element.overview}}</p>
             <p class="card-text m-0">Vote: {{element.vote_average}}</p>
             <p class="card-text m-0">Relase date: {{element.release_date}}</p>
+            <div class="d-flex align-items-center">
+                <p class="card-text m-0 me-2">Language: {{element.original_language}}</p>
+                <country-flag :country="getLanguage()" size='normal'/>
+            </div>
+
         </div>
     </div>
 </template>
@@ -24,6 +30,19 @@ export default {
                 return this.element.title;
             } else {
                 return this.element.name;
+            }
+        },
+
+        getLanguage(){
+            switch(this.element.original_language){
+                case 'en':
+                    return 'gb';
+                case 'ja':
+                    return 'jp';
+                case 'zh':
+                    return 'cn';
+                default:
+                    return this.element.original_language;
             }
         }
     }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import CountryFlag from 'vue-country-flag'
+Vue.component('country-flag', CountryFlag)
+
 Vue.config.productionTip = false
 
 new Vue({
