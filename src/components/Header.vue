6867939f54ed05f4aeda40bc5ccec67cc18ff991
MattiaPortanti <mattia.portantipm@gmail.com>
2022-05-02 17:53:01 +0200

    aggiunto keyup nella serch bar del header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,7 @@
             </div>
 
             <div class="input-group">
-                <input type="text" class="form-control" placeholder="Search movie.." v-model.trim="stringToSearch">
+                <input type="text" class="form-control" placeholder="Search movie.." v-model.trim="stringToSearch" @keydown.enter="$emit('newSearch', stringToSearch)">
                 <button class="btn btn-danger" type="button" id="button-addon2" @click="$emit('newSearch', stringToSearch)">Search</button>
             </div>
         </nav>
